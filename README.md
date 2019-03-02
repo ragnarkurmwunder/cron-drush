a74664fbd58fdb69edc156af9f3966bbbb49c75a
Ragnar Kurm <ragnar.kurm@wunder.io>
2019-03-03 01:52:48 +0200

    Refactor a bit. Make it more universal.

--- README.md
+++ README.md
@@ -8,19 +8,81 @@ Originally it has been developed in AWS context, so it may contain some specific
 
 1. Copy the script to your system `/usr/local/bin/drin-drush.sh`
 1. Give it execution rights `chmod 755 /usr/local/bin/cron-drush.sh`
-1. Modify it according to your needs
-1. Configure system cron entry
-   1. Create new cron file (for example `/etc/cron.d/drush`) or append to an existing one
-   1. Add an entry, for example:
-   ```
-   PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
-   0 * * * *  root  cron-drush.sh cron
-   ```
-1. This script assumes availbility of following environment variables for its operations:
-   1. `COMPOSER_HOME` (for example: `/usr/lib/composer`)
-   1. `BASE_URL` (optional) (for example: `https://www.example.com`)
-   1. `WEBROOT` (for example `/var/app/current/web`)
-1. Either define those in your cron file or modify the script to your needs
+1. Configure system cron (see below)
+1. Configure environment variables (see below)
+
+### System Cron
+
+1. Create new cron file (for example `/etc/cron.d/drush`)
+1. Add an entry (see example below)
+1. Configure environment variables
+
+#### Example
+
+```
+WEB_USER=apache2
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
+0 * * * *  root  cron-drush.sh cron
+```
+
+### Environment variables
+
+Those variables have to be specified in cron file,
+or in AWS case those can be also in `envvars`
+which is used automatically.
+
+#### CRON_DRUSH_LOG
+
+Optional.
+
+Location cron drush logfile.
+
+Default: `/var/log/cron-drush.log`
+
+#### WEB_USER
+
+Optional.
+
+Specifies web server user for running `drush` as.
+
+Default: tries to guess between some common usernames.
+
+#### COMPOSER_HOME
+
+Optional.
+
+This is needed for script to find `drush`.
+In composer dir there is `vendor/bin` which contains `drush`.
+
+Default: `/usr/lib/composer/vendor/bin/drush`
+
+#### WEBROOT
+
+Optional.
+
+Drush needs to know where Drupal is located.
+
+Default: tries to guess between some common paths.
+
+#### LEADER_SCRIPT
+
+Optional.
+
+This is needed only in multi-instance setup where there are multiple web servers against one database.
+The script determines by exit code 0 which instance is considered "leader" in the group.
+
+The value may be absolute path or relative path.
+In latter case the script is searched from PATH.
+
+Default: `leader.py`
+
+#### BASE_URL
+
+Optional.
+
+In makes sense only in single-site setup when some Drupal functionality may need it and creating drush aliases files is not needed.
+
+Example: `https://www.example.com`
 
 ## Usage
 
@@ -28,13 +90,7 @@ Originally it has been developed in AWS context, so it may contain some specific
 * This script is meant to be executed as root
 * It passes it's arguments to `drush`, so accepts arbitrary `drush` arguments, not just `cron`
 * See logs at `/var/log/cron-drush.log`
-
-## Modifications
-
-1. Take care of environment variables (mentioned in "Installation" section)
-1. Specify web server user and group (for `sudo` and `chmod`)
-1. Make sure the `PATH` (in the script) allows find `drush`, `mysql` and other base utilities
-1. If you don't need leader functionality, strip it out
+* Create leader script if needed.
 
 ## Running multi-site
 
--- cron-drush.sh
+++ cron-drush.sh
@@ -1,66 +1,234 @@
 #!/bin/bash
 
 # This script must be executed as root.
-# It will execute drush as webapp.
+# It will execute drush as web user.
 
-# export PATH after envvars, because envvars may distort PATH
-source /opt/elasticbeanstalk/support/envvars
-export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:$COMPOSER_HOME/vendor/bin"
+# Fail if error.
+set -e
 
-log=/var/log/cron-drush.log
 
-# make sure this script is run as webapp
-current_user=$(id -un)
-case "$current_user" in
-  root)
-    # webapp does not have home, so we must circumvent
+# AWS-specific: read envvars into current environment
+# It may contain important variables for us.
+function load_aws_envvars {
+  local envvars=/opt/elasticbeanstalk/support/envvars
+  if [ -f "$envvars" ]; then
+    source "$envvars"
+  fi
+}
 
-    # need to initiate log file as root
-    # because later, being as webapp, we lack permissions to do so
-    touch "$log"
-    chown webapp:webapp "$log"
 
-    sudo -u webapp "$0" "$@"
-    exit "$?"
-    ;;
-  webapp)
-    true
-    ;;
-  *)
-    echo "Execute this script as 'root'."
-    exit 1
-    ;;
-esac
+# Logging: If logfile is not not specified, provide defualt.
+function determine__cron_drush_log {
+  export CRON_DRUSH_LOG="${CRON_DRUSH_LOG:-/var/log/cron-drush.log}"
+}
 
-leader.py || exit
 
-exec >> "$log" 2>&1
+function determine__web_user {
 
-echo
-echo
-echo
-date
+  # Return if WEB_USER contains a good username.
+  if id -u "$WEB_USER" &>/dev/null; then
+    return
+  fi
 
-# create temporary dir
-# and remove it upon exiting
-# can safely drop there all temporary files
-tmp=$(mktemp -d /tmp/cron-drush.XXXXXXXX || exit 1)
-trap "rm -rf $tmp" EXIT
+  # Try to guess web user.
+  unset WEB_USER
+  local guess
+  for guess in webapp www-data apache
+  do
+    if id -u "$guess" &>/dev/null; then
+      export WEB_USER="$guess"
+      return
+    fi
+  done
 
-stats="$tmp/stats"
+  echo "Environment variable 'WEB_USER' must be set. Needed to run `drush` as proper user."
+  exit 1
+}
 
-if [ -n "$BASE_URL" ]; then
-  # for a single-site, we assume BASE_URL is defined
-  cmd=(drush --root="$WEBROOT" --uri="$BASE_URL" "$@")
-else
-  # for a multi-site, we assume BASE_URL is not defined in env, and it is supplied by an alias.
-  cmd=(drush --root="$WEBROOT" "$@")
-fi
 
-set -x
-/usr/bin/time -f 'time=%es, mem=%Mkb' -o "$stats" "${cmd[@]}"
-{ set +x; } 2> /dev/null
+function determine__composer_home {
 
-# log stats to syslog
-msg="$(cat "$stats"), cmd=${cmd[*]}"
-logger -t "drush-cron" -- "$msg"
+  # Check if drush is already in PATH.
+  if hash drush &>/dev/null; then
+    return
+  fi
+
+  # The variable is properly set.
+  if [ -f "$COMPOSER_HOME/vendor/bin/drush" ]; then
+    return
+  fi
+
+  # Try to guess.
+  if [ -f "/usr/lib/composer/vendor/bin/drush" ]; then
+    export COMPOSER_HOME=/usr/lib/composer/vendor/bin/drush
+    return
+  fi
+
+  echo "Environment variable 'COMPOSER_HOME' must be set. Expecting to find 'drush' under '$COMPOSER_HOME/vendor/bin'."
+  exit 1
+}
+
+
+function determine__webroot {
+
+  if [ -f "$WEBROOT/index.php" ]; then
+    return
+  fi
+
+  # Try to guess.
+  # For D7 and D8.
+  local guess
+  for guess in /{var/app/current,var/app/current/current,var/www,var/www/html,var/lib/www,var/apache,usr/local/httpd,Library/WebServer,Local/Library/WebServer,usr/local/apache2}/{.,web}
+  do
+    if [ -f "$guess/index.php" ]; then
+      export WEBROOT="$(readlink -f "$guess")"
+      return
+    fi
+  done
+
+  echo "Environment variable 'WEBROOT' must be set. 'drush' needs to know where Drupal is located."
+  exit 1
+}
+
+
+# Need to initiate log file as root,
+# because later, being as web user,
+# we lack permissions to do so.
+function init_logfile {
+   touch "$CRON_DRUSH_LOG"
+   chown "$WEB_USER" "$CRON_DRUSH_LOG"
+}
+
+
+# Make sure this script is run as web user.
+function switch_users {
+
+  local current_user="$(id -un)"
+
+  case "$current_user" in
+
+    root)
+      # Web user does not have home,
+      # so we must circumvent.
+      init_logfile
+      sudo -u "$WEB_USER" "$0" "$@"
+      exit "$?"
+      ;;
+
+    "$WEB_USER")
+      true
+      ;;
+
+    *)
+      echo "Execute this script as 'root'."
+      exit 1
+      ;;
+
+  esac
+}
+
+
+function determine__leader_script {
+
+  if [ -f "$LEADER_SCRIPT" ]; then
+    return
+  fi
+
+  # Note that backslash skips aliasing.
+  local guess="$(\which leader.py &>/dev/null || true)"
+  if [ -f "$guess" ]; then
+    export LEADER_SCRIPT="$guess"
+    return
+  fi
+
+  export -n LEADER_SCRIPT
+  unset LEADER_SCRIPT
+}
+
+
+function ensure_leader {
+  if [ -f "$LEADER_SCRIPT" ]; then
+    if ! "$LEADER_SCRIPT"; then
+      exit
+    fi
+  fi
+}
+
+
+function logging_start {
+
+  # Redirect stdout and stderr to append to a logfile.
+  exec &>>"$CRON_DRUSH_LOG"
+
+  # Add some space to log followed by timestamp.
+  echo
+  echo
+  echo
+  date
+}
+
+
+# Note, it creates global $cmd, because cannot return array.
+function determine_drush_command_line {
+  if [ -n "$BASE_URL" ]; then
+    # For a single-site, we assume BASE_URL is defined.
+    cmd=(drush --root="$WEBROOT" --uri="$BASE_URL" "$@")
+  else
+    # For a multi-site, we assume BASE_URL is not defined in env,
+    # and it is supplied by an alias.
+    cmd=(drush --root="$WEBROOT" "$@")
+  fi
+}
+
+
+# Debug/Log the actual drush command line executed.
+# It also generates stats.
+function execute_drush {
+  local stats="$1"
+  set -x
+  /usr/bin/time -f 'time=%es, mem=%Mkb' -o "$stats" "${cmd[@]}"
+  { set +x; } &>/dev/null
+}
+
+
+# Log stats to Syslog.
+function log_stats {
+  local msg="$(cat "$stats")"
+  # To syslog.
+  logger -t "drush-cron" -- "$msg"
+  # To our log.
+  echo "$msg"
+}
+
+
+function main {
+  load_aws_envvars
+  determine__composer_home
+  # Set PATH as early as possible, but after load_aws_envvars and determine__composer_home,
+  # because they mey distort or influence PATH.
+  export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:$COMPOSER_HOME/vendor/bin"
+  determine__cron_drush_log
+  determine__web_user
+  determine__webroot
+  # It also initialises logfile while root:
+  switch_users "$@"
+  # From here on, we are executing as web user.
+  determine__leader_script
+  ensure_leader
+  # Start logging after leader is determined:
+  logging_start
+  # Create temporary dir and remove it upon exiting.
+  # Cannot put it into a function, EXIT is emitted on return.
+  local tmp=$(mktemp -d /tmp/cron-drush.XXXXXXXX || exit 1)
+  trap "rm -rf $tmp" EXIT
+  stats="$tmp/stats"
+  # Note, it creates global $cmd, because cannot return array:
+  determine_drush_command_line "$@"
+  # Saves stats:
+  execute_drush "$stats"
+  # Reads stats and saves into log:
+  log_stats "$stats"
+}
+
+
+main "$@"
